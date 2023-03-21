b1acd06d55af566819e1ff8526c45bb18c42ea29
Rob King <rob@sendwithus.com>
2023-03-20 20:18:46 -0700

    initial commit and prework

---
+++ day1/clock-speed.c
@@ -0,0 +1,14 @@
+#include <time.h>
+#include <stdio.h>
+
+#define ITERS 1000000000
+
+int main(int argc, char **argv)
+{
+    clock_t start = clock();
+    for (int i = 0; i < ITERS; i++)
+        ;
+    float secs = (float)(clock() - start) / CLOCKS_PER_SEC;
+    float ops = ITERS / secs;
+    printf("Clock speed approx %.3f GHz\n", ops / 1000000000.0);
+}
